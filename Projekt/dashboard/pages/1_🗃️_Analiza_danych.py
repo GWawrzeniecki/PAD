d69fd6fce06c9966046c11be404be9b24f671b8c
Grzegorz Wawrzeniecki <grzegorz.wawrzeniecki@interia.pl>
2024-02-03 19:10:37 +0100

    works on mac

--- Projekt/dashboard/pages/1_🗃️_Analiza_danych.py
+++ Projekt/dashboard/pages/1_🗃️_Analiza_danych.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import subprocess
 
+
 # nie mogłem zaimportować utility-path errors
 def convert_to_markdown(notebook_path):
     # Convert the notebook to markdown
     subprocess.run(["jupyter", "nbconvert", "--to", "markdown", notebook_path])
 
+
 st.markdown("# Analiza i czyszczenie zbioru danych")
 
-# convert_to_markdown("Projekt/data_prepare.ipynb")
+convert_to_markdown("Projekt/data_prepare.ipynb")
 
 with open("Projekt/data_prepare.md", "r", encoding="utf-8") as f:
     markdown_content = f.read()
--- Projekt/dashboard/pages/3_🧮_Model_regresji.py
+++ Projekt/dashboard/pages/3_🧮_Model_regresji.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import subprocess
 
+
 # nie mogłem zaimportować utility-path errors
 def convert_to_markdown(notebook_path):
     # Convert the notebook to markdown
     subprocess.run(["jupyter", "nbconvert", "--to", "markdown", notebook_path])
 
+
 st.markdown("# Budowa modelu regresji")
 
-# convert_to_markdown("Projekt/regression_model.ipynb")
+convert_to_markdown("Projekt/regression_model.ipynb")
 
 with open("Projekt/regression_model.md", "r", encoding="utf-8") as f:
     markdown_content = f.read()
